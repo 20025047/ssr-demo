21da299be7e44f21f99d15f0e33a8c627d6dc6c6
binhqd <binhqd@vnpt.vn>
2024-08-19 13:39:02 +0700

    server side rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ssr-demo';
+  posts: any[] = [];
+  constructor(
+    private http: HttpClient
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    this.http.get(url).subscribe((res) => {
+      this.posts = res as any[];
+    });
+  }
 }
